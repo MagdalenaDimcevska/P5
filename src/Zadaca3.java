20ccc52672863fac3d624a9f25c159a892b826c1
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2024-01-11 22:59:07 +0100

    Add first java project

---
+++ src/Zadaca3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Zadaca3 {
+	public static void main(String[] args) {
+		int x=1, y=2, z=5;
+		if((x*y)>=z)
+		if(x>y)
+		System.out.print("3");
+		else 
+		System.out.print("4");
+		else 
+		System.out.print("5");
+		}
+	}
